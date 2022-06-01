7a322822809f995ec3bfeebe092083b798e07fcd
kristina.ushakova <kris3007199713@gmail.com>
2022-06-01 19:00:03 +0300

    Добавьте файлы проекта.

---
+++ LeetCode/Problems/Array/RemoveElement.cs
@@ -0,0 +1,25 @@
+﻿/*using System;
+
+namespace Array;
+
+public class RemoveElement
+{
+    public static void Main(string[] args)
+    {
+        removeElement();
+    }
+    public int removeElement(int[] nums, int val)
+    {
+        int i = 0;
+        for (int k = 0; k < nums.Length; k++)
+        {
+            if (nums[k] != val)
+            {
+                nums[i] = nums[k];
+                i++;
+            }
+        }
+        return i;
+    }
+
+}*/
---
+++ LeetCode/Problems/Array/Two Sum.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int[]? TwoSum(int[] nums, int target)
+    {
+
+        for (int k = 0; k < nums.Length; k++)
+        {
+            int lt = 0; // первый, то есть левый
+            int rt = nums.Length - 1; // второй, то есть правый
+            int sum = nums[lt] + nums[rt];
+            if (target == sum)
+            {
+                return new int[] { nums[lt], nums[rt] };
+            }
+        }
+        return null;
+    }
+}
